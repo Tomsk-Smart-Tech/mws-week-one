1eb9e8edff9a782e7b61e07b529ed991e37d0693
Pavel Ryzhenkov <mpaasha4@gmail.com>
2026-04-06 05:57:56 -0500

    initial commit

---
+++ backend/mws-wiki/src/main/java/com/tst/mwswiki/MwsWikiApplication.java
@@ -0,0 +1,13 @@
+package com.tst.mwswiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwsWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwsWikiApplication.class, args);
+    }
+
+}
---
+++ backend/mws-wiki/src/test/java/com/tst/mwswiki/MwsWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tst.mwswiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwsWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
